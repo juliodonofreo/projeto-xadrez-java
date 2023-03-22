92c964aef0d8043015fffe68937dce068cbe781b
Julio Donofreo <julio.bibico@hotmail.com>
2023-03-22 14:45:46 -0300

    implementada classe de posição

--- src/aplicacao/Main.java
+++ src/aplicacao/Main.java
@@ -1,7 +1,10 @@
 package aplicacao;
 
+import jogoTabuleiro.Posicao;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Posicao ps = new Posicao(3, 5);
+        System.out.println(ps);
     }
 }
